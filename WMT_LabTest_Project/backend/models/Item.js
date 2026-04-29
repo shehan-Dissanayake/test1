edecf97ff0dccc373f250e624d97156df05fcf3a
IT24104090 <shehanr900@gmail.com>
2026-04-30 00:46:29 +0530

    commit

---
+++ WMT_LabTest_Project/backend/models/Item.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: [0, "Rating cannot be negative"],
+      max: [5, "Rating cannot be Graterthan 5"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
